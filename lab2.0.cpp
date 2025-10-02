e66343c7778591a562bd0656731dbe7fa6cb94b1
Dmytroqght <dmytro.ganja.kn@dspu.ua>
2025-10-02 16:30:39 +0300

    commit 3

--- lab2.0.cpp
+++ lab2.0.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 using namespace std;
+
+void Hello() {
+	cout << "Hello cpp" << endl;
+}
+
 int main()
 {
 	double x;
@@ -10,6 +15,7 @@ int main()
 	cout << "x = "; cin >> x;
 	z1 = 2 * x;
 	z2 = x + x;
+	Hello();
 	cout << endl;
 	cout << "Result22: " << endl;
 	cout << "z_1 = " << z1 << endl;
